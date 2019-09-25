daea28914445c98a8e6716f43e182b17195f751e
nguyen tri nhan ho <nhan01628483955@gmail.com>
2019-09-25 23:10:15 +0700

    chức năng login cơ bản

---
+++ LibraryManagement/LibraryManagement/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LibraryManagement
+{
+    class LoginSupports
+    {
+        public static void RequestLogin()
+        {
+            Console.Write("Username : ");
+            string username = Console.ReadLine();
+            Console.Write("Password : ");
+            HideInputCharacter();
+            string password = Console.ReadLine();
+            ResetInputCharacter();
+            if (TryToLogin(username, password))
+            {
+                Console.WriteLine("Login complete successfully");
+            }else
+            {
+                Console.WriteLine("Login failed");
+                Console.WriteLine("Please re-input info to login");
+                RequestLogin();
+            }
+        }
+
+        private static bool TryToLogin(string username, string password)
+        {
+            return username == "sa" && password == "123456Az";
+        }
+
+        public static void HideInputCharacter()
+        {
+            Console.ForegroundColor = Console.BackgroundColor;
+        }
+
+        public static void ResetInputCharacter()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ LibraryManagement/LibraryManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace LibraryManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Welcome you to Library Management Tool");
+            Console.WriteLine("First you need to login");
+            LibraryManagement.LoginSupports.RequestLogin();
+        }
+    }
+}
