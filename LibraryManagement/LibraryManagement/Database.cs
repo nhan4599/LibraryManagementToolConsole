f98e4f1d3a42b01a335afb1b6e47657647732157
nguyen tri nhan ho <nhan01628483955@gmail.com>
2019-09-29 08:23:53 +0700

    temp

---
+++ LibraryManagement/LibraryManagement/Database.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+namespace LibraryManagement
+{
+    class Database
+    {
+        private const string connectionString = "Data Source=DESKTOP-ARHTHJK;Initial Catalog=QLNhanVien;User ID=sa;Password=nhan4599";
+        public static bool GetUserIsDuplicated(string user)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+
+            }
+            return false;
+        }
+    }
+}
